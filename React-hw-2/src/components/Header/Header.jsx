733b3d7e4d831da304b549b879846163b09470d2
DmytroBrazhnyk <139158565+DmytroBrazhnyk@users.noreply.github.com>
2024-02-22 03:24:22 +0200

    fix some bug hw2

--- React-hw-2/src/components/Header/Header.jsx
+++ React-hw-2/src/components/Header/Header.jsx
@@ -7,7 +7,7 @@ export default function Header({ cartCount, favoriteCount }){
             <div className="header_logo">BeerKongo</div>
             <div className='header_info'>
                 <div className="cart">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                     <circle cx="10" cy="20" r="1"></circle>
                     <circle cx="18" cy="20" r="1"></circle>
                     <path d="M1 1h4l2.68 13.39a2 2 0 0 0 1.92 1.61H18"></path>
@@ -16,7 +16,7 @@ export default function Header({ cartCount, favoriteCount }){
                     <p>{cartCount}</p>
                 </div>
                 <div className="favorite">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                         <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2" />
                     </svg>
                     <p>{favoriteCount}</p>
@@ -26,7 +26,7 @@ export default function Header({ cartCount, favoriteCount }){
     );
 }
 
-Header.PropTypes ={
+Header.propTypes ={
     cartCount : PropTypes.number.isRequired,
     favoriteCount : PropTypes.number.isRequired
 };
